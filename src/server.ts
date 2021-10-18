eb30396fc4488c640638f72676a0a16458984951
vvkin <vkincur@gmail.com>
2021-10-18 23:39:39 +0300

    Set up basic project layout

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import Fastify from 'fastify';
+
+const server = Fastify({ logger: true });
+
+server.get('/', (request, reply) => {
+    reply.send({ hello: 'world' });
+});
+
+server.listen(3000, (err) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+});
