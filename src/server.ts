8bee0c848dc1b30e37398596c6a890fab6751a7c
ShkarupaDC <dimon.shkarupa@gmail.com>
2021-10-19 23:56:48 +0300

    Add linter, formatter and pre-commit, pre-push hooks

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import Fastify from 'fastify';
 const server = Fastify({ logger: true });
 
 server.get('/', (request, reply) => {
-    reply.send({ hello: 'world' });
+  reply.send({ hello: 'world' });
 });
 
 server.listen(3000, (err) => {
